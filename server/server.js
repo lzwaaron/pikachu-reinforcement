3500943966bdac62829eb98258955fa3d1f8ca90
lzwaaron <lzwaaron@gmail.com>
2023-09-18 23:49:56 -0400

    initial setup

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const cors = require('cors');
+
+const port = 3000;
+
+// const apiRouter = require('./routes/apiRouter');
+
+// Define your trusted origins for CORS
+const allowedOrigins = [
+  'http://localhost:3000',
+  // 'https://your-production-domain.com',
+];
+
+
+// added cors and setHeader to try to resolve the "Cross-Origin-Opener-Policy policy would block the window.closed call." error in chrome console after oAuth, but no luck yet
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+    next();
+});
+
+app.use(express.json());
+
+
+// serve static assets from the 'build' directory
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// serve index.html for any unmatched route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const nodePolyFillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    port: 8080,
+    static: [
+      {
+        directory: path.join(__dirname, 'build'),
+        publicPath: '/',
+      },
+    ],
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+
+  entry: path.join(__dirname, 'index.js'),
+  devtool: 'eval-source-map', // added to resolve the dev tool error in chrome console
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  mode: process.env.NODE_ENV,
+
+  plugins: [
+    new htmlWebpackPlugin({ template: './index.html' }),
+    new nodePolyFillPlugin(),
+  ],
+
+  module: {
+    rules: [
+      // for TSX files
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        // for JS & JSX files
+        test: /.(js|jsx)$/,
+        exclude: path.resolve(__dirname, 'node_modules'), // there was a typo here (node-modules)
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      {
+        // for CSS/SASS files
+        test: /.(css|scss)$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
